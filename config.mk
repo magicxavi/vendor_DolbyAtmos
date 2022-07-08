d7aa8fd9ec66279595c495eb16cc9fdb41381c7b
Magicxavi <rolfpeterkesche@gmail.com>
2022-07-08 20:55:43 +0300

    import dolby props

--- config.mk
+++ config.mk
@@ -1,2 +1,12 @@
 $(call inherit-product, vendor/DolbyAtmos/common/common-vendor.mk)
 
+# Dolby    
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.feature.dolby_enable=true \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    persist.vendor.audio_fx.current=dolby
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=16
